ed6cb90e2c6ac3a84003ce9d754c23575e91d7c0
JaeHyunGround <gothddlek@naver.com>
2024-10-17 14:41:13 +0900

    feat(app): 문자열 입력 기능 구현
    프로그램 실행 시, '덧셈할 문자열을 입력해 주세요.' 문구가 출력되고 사용자가 문자열을 입력 후, 결과과 출력되는 기능을 구현했습니다.
    
    Promise를 사용하여 사용자 입력이 끝나기 전까지 다음 동작이 실행되지 않도록 구현했습니다.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,22 @@
+import readline from "readline";
+
 class App {
-  async run() {}
+  async run() {
+    const rl = readline.createInterface({
+      input: process.stdin,
+      output: process.stdout,
+    });
+
+    const input = await new Promise((resolve) => {
+      console.log("덧셈할 문자열을 입력해주세요.");
+      rl.on("line", (line) => {
+        resolve(line);
+        rl.close();
+      });
+    });
+
+    console.log(`결과 : ${input}`);
+  }
 }
 
 export default App;
